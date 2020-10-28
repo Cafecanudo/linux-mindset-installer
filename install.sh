975e0b09256f20c035104a3daa1527c6967bd2a4
Wellton S. Barros <cafecanudo@gmail.com>
2020-10-28 00:21:06 -0300

    adicionando ? para perguntar se bash deve ser instalado ou nao

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #configs
 apt upgrade
 
@@ -9,14 +11,33 @@ APT=_apt.sh
 chmod +x $APT
 ./$APT
 
-FILES=$(ls | grep -v "_" | grep -v "install.sh")
-for FILE in $FILES;
+execute_install () {
+	echo ""
+	echo "###################################################################"
+	echo "###### Instalando $1"
+	echo "####################################################################"
+	sudo chmod +x $1
+	sudo ./$1
+	echo "####################################################################"
+}
+
+for FILE in *;
 do
-	echo "\n###################################################################"
-	echo "###### Instalando $FILE"
-	echo "####################################################################"
-	sudo chmod +x $FILE
-	sudo ./$FILE
-	echo "####################################################################"
+	FILE_NAME=$(echo $FILE | grep -vEi "_|install\.sh" | egrep -c "\.sh$")
+	if [ $FILE_NAME == 1 ] #verificar se arquivo pode ser executado
+	then
+		question=$(echo $FILE | grep -c "?")
+		if [ $question == 1 ] #Verificar se o arquivo deve ser questionado para instalar
+		then
+			echo "Deseja instalar \"$FILE\"? (y) (n)"
+			read installOrNotInstall
+			if [ $installOrNotInstall == 'y' ]
+			then
+				execute_install $FILE
+			fi
+		else
+			execute_install $FILE
+		fi
+	fi
 done 
 
